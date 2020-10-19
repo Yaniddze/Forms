aeba4bba6a8ce31456ff47ac61e27d61712b8a00
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 21:20:08 +0500

    :wrench: Create create forms handler

--- Back/Api/Storage/Abstractions/CreateForm.cs
+++ Back/Api/Storage/Abstractions/CreateForm.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Api.Domain;
 using Api.UseCases.Abstractions;
 
 namespace Api.Storage.Abstractions
 {
     public interface CreateForm
     {
-        Task<AbstractAnswer<Guid>> HandleAsync();
+        Task<AbstractAnswer<Guid>> HandleAsync(IEnumerable<Field> fields, string[] keywords);
     }
 }
---
+++ Back/Api/Storage/Handlers/CreateFormHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain;
+using Api.Storage.Abstractions;
+using Api.Storage.DbEntities;
+using Api.UseCases.Abstractions;
+using AutoMapper;
+using static Api.UseCases.Abstractions.AbstractAnswer<System.Guid>;
+
+namespace Api.Storage.Handlers
+{
+    public class CreateFormHandler: CreateForm
+    {
+        private readonly ApiContext context;
+        private readonly IMapper mapper;
+
+        public CreateFormHandler(ApiContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<AbstractAnswer<Guid>> HandleAsync(IEnumerable<Field> fields, string[] keywords)
+        {
+            try
+            {
+                var mappedFields = fields.Select(x => mapper.Map<Field, FieldDB>(x));
+                var tempGuid = Guid.NewGuid();
+
+                context.Forms.Add(new FormDB
+                {
+                    Id = tempGuid,
+                    Fields = mappedFields,
+                    Keywords = keywords,
+                });
+            
+                return CreateSuccess(tempGuid);
+            }
+            catch
+            {
+                return CreateFailed(new[] {"Database error"});
+            }
+        }
+    }
+}
