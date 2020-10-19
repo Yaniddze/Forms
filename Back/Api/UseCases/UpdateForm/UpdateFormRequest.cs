bd569854b2faf1d24028e39d2662d313c046ce27
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 22:49:10 +0500

    :sparkles: Add form updating use case

---
+++ Back/Api/UseCases/UpdateForm/UpdateFormRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Api.Domain;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.UpdateForm
+{
+    public class UpdateFormRequest: IRequest<AbstractAnswer<Form>>
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Field> Fields { get; set; }
+    }
+}
---
+++ Back/Api/UseCases/UpdateForm/UpdateFormUseCase.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Api.Domain;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.UpdateForm
+{
+    public class UpdateFormUseCase: IRequestHandler<UpdateFormRequest, AbstractAnswer<Form>>
+    {
+        private readonly Abstractions.UpdateForm formUpdater;
+        private readonly GetObjectKeys keysGetter;
+        
+        public UpdateFormUseCase(Abstractions.UpdateForm formUpdater, GetObjectKeys keysGetter)
+        {
+            this.formUpdater = formUpdater;
+            this.keysGetter = keysGetter;
+        }
+
+        public async Task<AbstractAnswer<Form>> Handle(UpdateFormRequest request, CancellationToken cancellationToken)
+        {
+            var keywords = keysGetter.Handle(request.Fields);
+
+            return await formUpdater.HandleAsync(new Form
+            {
+                Fields = request.Fields,
+                Id = request.Id,
+                Keywords = keywords,
+            });
+        }
+    }
+}
