6855f947e82686a12a6d7bc6258b5ffa1e92ad8b
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 19:28:02 +0500

    :wrench: Add services

---
+++ Back/Api/ServiceInstallers/Services/MediatRInstaller.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceInstallers.Services
+{
+    public class MediatRInstaller: ServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(typeof(Startup));
+        }
+    }
+}
---
+++ Back/Api/ServiceInstallers/Services/RouteInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceInstallers.Services
+{
+    public class RouteInstaller: ServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+        }
+    }
+}
