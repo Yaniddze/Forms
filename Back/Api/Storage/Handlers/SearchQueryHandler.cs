a630a35d2bdf2c062f016ffe8d8d0ceaacb7c238
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 21:41:23 +0500

    :wrench: Create search query handler

---
+++ Back/Api/Storage/Handlers/SearchQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Domain;
+using Api.Storage.Abstractions;
+using Api.Storage.DbEntities;
+using Api.UseCases.Abstractions;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using static Api.UseCases.Abstractions.AbstractAnswer<System.Collections.Generic.IEnumerable<Api.Domain.Form>>;
+
+namespace Api.Storage.Handlers
+{
+    public class SearchQueryHandler: SearchQuery
+    {
+        private readonly ApiContext context;
+        private readonly IMapper mapper;
+
+        public SearchQueryHandler(IMapper mapper, ApiContext context)
+        {
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        public async Task<AbstractAnswer<IEnumerable<Form>>> HandleAsync(string query)
+        {
+            try
+            {
+                var founded = await context.Forms
+                    .Where(x => x.Keywords.Contains(query))
+                    .Select(x => mapper.Map<FormDB, Form>(x))
+                    .ToListAsync();
+
+                return CreateSuccess(founded);
+            }
+            catch
+            {
+                return CreateFailed(new [] {"Database error"});
+            }
+        }
+    }
+}
