d5617c206a2224ebd4d4248d881b9acfa8a60ea4
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 22:57:19 +0500

    :sparkles: Add forms getter use case

---
+++ Back/Api/UseCases/GetForms/GetFormUseCase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.Domain;
+using Api.UseCases.Abstractions;
+using MediatR;
+using static Api.UseCases.Abstractions.AbstractAnswer<System.Collections.Generic.IEnumerable<Api.Domain.Form>>;
+
+namespace Api.UseCases.GetForms
+{
+    public class GetFormUseCase: IRequestHandler<GetFormsRequest, AbstractAnswer<IEnumerable<Form>>>
+    {
+        private readonly GetAllForms formGetter;
+
+        public GetFormUseCase(GetAllForms formGetter)
+        {
+            this.formGetter = formGetter;
+        }
+
+        public async Task<AbstractAnswer<IEnumerable<Form>>> Handle(GetFormsRequest request, CancellationToken cancellationToken)
+        {
+            if (request.Count <= 0 || request.Offset < 0)
+            {
+                return CreateFailed(new [] {"Count and Offset must be positive"});            
+            }
+
+            return await formGetter.HandleAsync(request.Count, request.Offset);
+        }
+    }
+}
---
+++ Back/Api/UseCases/GetForms/GetFormsRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Api.Domain;
+using Api.UseCases.Abstractions;
+using MediatR;
+
+namespace Api.UseCases.GetForms
+{
+    public class GetFormsRequest : IRequest<AbstractAnswer<IEnumerable<Form>>>
+    {
+        public int Count { get; set; }
+        public int Offset { get; set; }
+    }
+}
