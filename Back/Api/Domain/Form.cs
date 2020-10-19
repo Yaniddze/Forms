6fcc82bfcf78f7e8a58720bccb2d3e76e01444af
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 19:48:57 +0500

    :sparkles: Create form domain object

---
+++ Back/Api/Domain/Form.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Api.Domain
+{
+    public class Form
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Object> Fields { get; set; }
+        public string[] Keywords { get; set; }
+    }
+}
