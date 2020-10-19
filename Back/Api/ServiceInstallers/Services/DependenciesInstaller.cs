549e4f1b9b43d833d062ba07e4994595b1e61c7f
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 21:43:40 +0500

    :wrench: Inject handlers

---
+++ Back/Api/ServiceInstallers/Services/DependenciesInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Api.Storage.Abstractions;
+using Api.Storage.Handlers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceInstallers.Services
+{
+    public class DependenciesInstaller: ServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<GetAllForms, GetAllFormsHandler>();
+            services.AddScoped<UpdateForm, UpdateFormHandler>();
+            services.AddScoped<DeleteForm, DeleteFormHandler>();
+            services.AddScoped<CreateForm, CreateFormHandler>();
+            services.AddScoped<SearchQuery, SearchQueryHandler>();
+        }
+    }
+}
