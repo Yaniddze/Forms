f3c1b13f4a617c97b9f5487921d5f536050cb753
Zinevich Yan <facecrack1337@gmail.com>
2020-10-19 20:51:49 +0500

    :sparkles: Add initial migration

---
+++ Back/Api/ServiceInstallers/Services/EfInstaller.cs
@@ -0,0 +1,20 @@
+﻿using Api.Storage;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.ServiceInstallers.Services
+{
+    public class EfInstaller: ServiceInstaller
+    {
+        public void Install(IServiceCollection services, IConfiguration configuration)
+        {
+            var conn = configuration.GetConnectionString("Postgres");
+
+            services.AddDbContextPool<ApiContext>(x =>
+            {
+                x.UseNpgsql(conn);
+            });
+        }
+    }
+}
---
+++ Back/Api/Storage/Migrations/ApiContextMigrations/20201019155114_InitialMigration.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Storage.Migrations.ApiContextMigrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "forms",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    keywords = table.Column<string[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_forms", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "fields",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    name = table.Column<string>(nullable: true),
+                    value = table.Column<string>(nullable: true),
+                    FormId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fields", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_fields_forms_FormId",
+                        column: x => x.FormId,
+                        principalTable: "forms",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_fields_FormId",
+                table: "fields",
+                column: "FormId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fields");
+
+            migrationBuilder.DropTable(
+                name: "forms");
+        }
+    }
+}
