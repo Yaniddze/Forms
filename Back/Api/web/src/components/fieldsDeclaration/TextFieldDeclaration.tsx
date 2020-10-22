048fad578b913eae8ee2ff8c721179eb600c2533
Zinevich Yan <facecrack1337@gmail.com>
2020-10-23 02:19:02 +0500

    :wrench: Add text field title validation

--- Back/Api/web/src/components/fieldsDeclaration/TextFieldDeclaration.tsx
+++ Back/Api/web/src/components/fieldsDeclaration/TextFieldDeclaration.tsx
@@ -4,33 +4,52 @@ import React, {
   useEffect,
 } from 'react';
 import { Form } from 'react-bootstrap';
+import { TitleValidation } from '../../domain';
 
 type PropTypes = {
   children?: never;
   title: string;
   onTitleChange: (newTitle: string) => void;
   onValueChange: (newValue: string) => void;
+  onValidChange: (newValue: boolean) => void;
 }
 
 export const TextFieldDeclaration: FC<PropTypes> = (
-  { title, onTitleChange, onValueChange }: PropTypes,
+  { 
+    title, 
+    onTitleChange, 
+    onValueChange,
+    onValidChange, 
+  }: PropTypes,
 ) => { 
   useEffect(() => {
     onValueChange('');
+
+    onValidChange(false);
   }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => { 
+    if (e.target.value !== null) {
+      onTitleChange(e.target.value);
+      onValidChange(TitleValidation(e.target.value).valid);
+    }
+  };
+
+  const { valid, message } = TitleValidation(title);
 
   return (
     <Form.Group>
       <Form.Label>Text. Название:</Form.Label>
       <Form.Control 
+        isValid={valid}
+        isInvalid={!valid}
         type="text"
         value={title}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          if (e.target.value !== null) {
-            onTitleChange(e.target.value);
-          }
-        }}
+        onChange={handleChange}
       />
+      <Form.Control.Feedback type="invalid">
+        {message}
+      </Form.Control.Feedback>
     </Form.Group>
   );
 };
