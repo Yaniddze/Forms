ccfb3be526b95e60ed55909a818d02d2ba8b762f
Zinevich Yan <facecrack1337@gmail.com>
2020-10-23 20:39:29 +0500

    :wrench: Add storage types

---
+++ Back/Api/web/src/storage/types.ts
@@ -0,0 +1,32 @@
+import { Form } from '../domain';
+
+export type StorageAnswer<TData> = {
+  success: boolean;
+  errors: string[];
+  data: TData;
+}
+
+export interface SaveForm {
+  Handle(fields: Record<string, unknown>): Promise<StorageAnswer<string>>;
+  Cancel(): void;
+}
+
+export interface UpdateForm {
+  Handle(form: Form): Promise<StorageAnswer<Form>>;
+  Cancel(): void;
+}
+
+export interface DeleteForm {
+  Handle(id: string): Promise<StorageAnswer<string>>;
+  Cancel(): void;
+}
+
+export interface GetForms {
+  Handle(count: number, offset: number): Promise<Form[]>;
+  Cancel(): void;
+}
+
+export interface Search {
+  Handle(query: string, count: number, offset: number): Promise<Form[]>;
+  Cancel(): void;
+}
